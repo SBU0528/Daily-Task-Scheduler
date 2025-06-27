4dc7d2040b90a30f2b251c7ba60495df96c46f9d
Sibusiso Botya <botyasibusiso@gmail.com>
2025-06-27 15:21:46 +0200

    update firebase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getFunctions } from 'firebase/functions';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyDrCPNk9NcPtDaVtogDg3C_vNT84prfk5U",
+  authDomain: "daily-task-scheduler-b1943.firebaseapp.com",
+  projectId: "daily-task-scheduler-b1943",
+  storageBucket: "daily-task-scheduler-b1943.appspot.com",
+  messagingSenderId: "1056285349774",
+  appId: "1:1056285349774:web" // Replace with your actual App ID if it differs
 };
 
 // Initialize Firebase
